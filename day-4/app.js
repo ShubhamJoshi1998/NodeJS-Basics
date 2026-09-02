3ace2148f8295fd3ce2cb13099f3e723894a02bc
shubham <shivam306joshi@gmail.com>
2026-09-02 14:54:37 +0530

    day4 - Event Loop Asyn Thinking

--- day-4/app.js
+++ day-4/app.js
@@ -1,6 +1,62 @@
-console.log("1");
-setTimeout(() => {
-    console.log("2");
-}, 0);
+// console.log("1");
+// setTimeout(() => {
+//     console.log("2");
+// }, 0);
 
-console.log("3");
+// console.log("3");
+
+// console.log("Start");
+// setTimeout(() => {
+//     console.log("Timer");
+// }, 0);
+// console.log("End");
+
+// console.log("A");
+
+// setTimeout(() => {
+//     console.log("B");
+// }, 0);
+
+// setTimeout(() => {
+//     console.log("C");
+// }, 1000);
+
+// console.log("D");
+
+// console.log("Start");
+
+// setTimeout(() => {
+//     console.log("Timer 1");
+// }, 3000);
+
+// setTimeout(() => {
+//     console.log("Timer 2");
+// }, 1000);
+
+// setTimeout(() => {
+//     console.log("Timer 3");
+// }, 0);
+
+// console.log("End");
+
+// Output : Start
+// End
+// Timer 3
+// Timer 2
+// Timer 1
+
+const fs = require("fs");
+
+console.log("Start");
+
+fs.readFile("data.txt", "utf-8",(err,data) => {
+    if(err){
+        console.log("Error Reading File");
+        return;
+    }
+    console.log("File COntent : " , data);
+})
+
+console.log("End");
+
+ 
